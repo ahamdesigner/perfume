658cff4d8dfb867fec38bca9d10d249d27c96a19
Syamnath <syamnathsnair33@gmail.com>
2021-02-05 09:26:27 +0530

    Update footgear.js
    അ

--- footgear.js
+++ footgear.js
@@ -1,5 +1,24 @@
+window.onload=function() {
 let today = new Date();
 function getDate(){
     console.info('.notes',today);
 }
 getDate()
+
+console.log('fine')
+
+ let paytrigger = document.getElementById('cash')
+
+ paytrigger.onchange=function() {
+
+  if(this.checked = true) {
+
+  document.getElementById("payment").removeAttribute('hidden')
+
+ } else {
+
+  document.getElementById("payment").setAttribute('hidden')
+
+  }
+ } 
+}
