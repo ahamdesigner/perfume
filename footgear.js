07459a35f27514a816852046d2001027e0258a96
Syamnath <syamnathsnair33@gmail.com>
2021-02-05 10:35:07 +0530

    Update footgear.js
    log

--- footgear.js
+++ footgear.js
@@ -4,8 +4,21 @@ function getDate(){
     console.info('.notes',today);
 }
 getDate()
-
 console.log('fine')
+
+(function () {
+    var old = console.log;
+    var logger = document.getElementById('log');
+    console.log = function () {
+      for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] == 'object') {
+            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 2) : arguments[i]) + '<br />';
+        } else {
+            logger.innerHTML += arguments[i] + '<br />';
+        }
+      }
+    }
+})();
 
  let paytrigger = document.getElementById('cash')
 
