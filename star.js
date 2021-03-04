45cd595f29643c19abc1f72b04ec65e566f77f63
Syamnath <syamnathsnair33@gmail.com>
2021-03-04 20:41:25 +0530

    Update star.js

--- star.js
+++ star.js
@@ -1,5 +1,6 @@
+const ₹ = 'revenue'
 // scent identify the contents
-window.onload=function() {
+window.onload=function(₹) {
 // Make the console available in the Pwa itself
 (function () {
     let old = console.log;
